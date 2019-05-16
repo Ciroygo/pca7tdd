3ada9cabc6c009af6e2c05c55277760c10c6cfbf
vagrant <vagrant@homestead>
2019-05-16 08:35:08 +0000

    21 全局作用域与查询重构

--- app/Reply.php
+++ app/Reply.php
@@ -7,7 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 class Reply extends Model
 {
     //
+    use Favoritable;
+    
     protected $guarded = [];
+    protected $with = ['owner', 'favorites'];
 
 
     public function owner()
@@ -15,23 +18,5 @@ class Reply extends Model
         return $this->belongsTo(User::class, 'user_id');
     }
 
-    public function favorites()
-    {
-        return $this->morphMany(Favorite::class, 'favorited');
-    }
 
-    public function favorite()
-    {
-//        return $this->favorites()->create(['user_id'=>auth()->id()]);
-        $attributes = ['user_id' => auth()->id()];
-
-        if (! $this->favorites()->where($attributes)->exists()){
-            return $this->favorites()->create($attributes);
-        }
-    }
-
-    public function isFavorited()
-    {
-        return $this->favorites()->where('user_id', auth()->id())->exists();
-    }
 }
--- app/Thread.php
+++ app/Thread.php
@@ -9,6 +9,8 @@ class Thread extends Model
     //
     protected $guarded = [];
 
+    protected $with = ['creator', 'channel'];
+
     public function path()
     {
         return "/threads/{$this->channel->slug}/{$this->id}";
@@ -16,7 +18,7 @@ class Thread extends Model
 
     public function replies()
     {
-        return $this->hasMany(Reply::class)->withCount('favorites')->with('owner');
+        return $this->hasMany(Reply::class);
     }
 
     public function creator()
